53daeb5c0811eeffeddaab44e4ac6085dfd8fc08
alvinle2901 <bach938@gmail.com>
2023-10-18 11:09:26 +0300

    basic UI for HomeScreen done

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,16 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+
+
+import { destinationData } from '../constants/index'
+import RecipeCard from './RecipeCard'
 
 const Recipes = () => {
   return (
-    <View className="mx-4 flex-row justify-between">
-      {
-        
-      }
+    <View className="mx-4 flex-row justify-between flex-wrap">
+      {destinationData.map((item, index) => {
+        return <RecipeCard item={item} key={index} />
+      })}
     </View>
   )
 }
 
 export default Recipes
-
-const styles = StyleSheet.create({})
