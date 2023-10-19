0de3f974a590eb47d1ea1c7c2997b35b084d684d
alvinle2901 <bach938@gmail.com>
2023-10-19 17:34:21 +0300

    added images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const destinationData = [
   {
     title: 'Kodak Portra 160',
-    image: require('../../assets/hotel.png'),
+    image: require('../../assets/films/portra160/1.webp'),
     specs: {
       'film simulation': 'Classic Chrome',
       'dynamic range': 'DR400',
@@ -17,96 +17,37 @@ export const destinationData = [
       clarity: '-3',
       iso: 'Auto',
       'exposure compensation': '+1'
-    }
+    },
+    images: [
+      require('../../assets/films/portra160/1.webp'),
+      require('../../assets/films/portra160/2.webp'),
+      require('../../assets/films/portra160/3.webp')
+    ]
   },
-//   {
-//     title: 'Kodak Portra 160',
-//     'film simulation': 'Classic Chrome',
-//     'dynamic range': 'DR400',
-//     'grain effect': 'Weak, Small',
-//     'color chrome effect': 'Strong',
-//     'color chrome fx blue': 'Strong',
-//     'white balance': '4900K, +4 Red & -6 Blue',
-//     highlight: '-1',
-//     shadow: '-2',
-//     color: '0',
-//     sharpness: '-2',
-//     nr: '-4',
-//     clarity: '-3',
-//     iso: 'Auto',
-//     'exposure compensation': '+1',
-//     image: require('../../assets/hotel.png')
-//   },
-//   {
-//     title: 'Kodak Portra 160',
-//     'film simulation': 'Classic Chrome',
-//     'dynamic range': 'DR400',
-//     'grain effect': 'Weak, Small',
-//     'color chrome effect': 'Strong',
-//     'color chrome fx blue': 'Strong',
-//     'white balance': '4900K, +4 Red & -6 Blue',
-//     highlight: '-1',
-//     shadow: '-2',
-//     color: '0',
-//     sharpness: '-2',
-//     nr: '-4',
-//     clarity: '-3',
-//     iso: 'Auto',
-//     'exposure compensation': '+1',
-//     image: require('../../assets/hotel.png')
-//   },
-//   {
-//     title: 'Kodak Portra 160',
-//     'film simulation': 'Classic Chrome',
-//     'dynamic range': 'DR400',
-//     'grain effect': 'Weak, Small',
-//     'color chrome effect': 'Strong',
-//     'color chrome fx blue': 'Strong',
-//     'white balance': '4900K, +4 Red & -6 Blue',
-//     highlight: '-1',
-//     shadow: '-2',
-//     color: '0',
-//     sharpness: '-2',
-//     nr: '-4',
-//     clarity: '-3',
-//     iso: 'Auto',
-//     'exposure compensation': '+1',
-//     image: require('../../assets/hotel.png')
-//   },
-//   {
-//     title: 'Kodak Portra 160',
-//     'film simulation': 'Classic Chrome',
-//     'dynamic range': 'DR400',
-//     'grain effect': 'Weak, Small',
-//     'color chrome effect': 'Strong',
-//     'color chrome fx blue': 'Strong',
-//     'white balance': '4900K, +4 Red & -6 Blue',
-//     highlight: '-1',
-//     shadow: '-2',
-//     color: '0',
-//     sharpness: '-2',
-//     nr: '-4',
-//     clarity: '-3',
-//     iso: 'Auto',
-//     'exposure compensation': '+1',
-//     image: require('../../assets/hotel.png')
-//   },
-//   {
-//     title: 'Kodak Portra 160',
-//     'film simulation': 'Classic Chrome',
-//     'dynamic range': 'DR400',
-//     'grain effect': 'Weak, Small',
-//     'color chrome effect': 'Strong',
-//     'color chrome fx blue': 'Strong',
-//     'white balance': '4900K, +4 Red & -6 Blue',
-//     highlight: '-1',
-//     shadow: '-2',
-//     color: '0',
-//     sharpness: '-2',
-//     nr: '-4',
-//     clarity: '-3',
-//     iso: 'Auto',
-//     'exposure compensation': '+1',
-//     image: require('../../assets/hotel.png')
-//   }
+  {
+    title: 'Kodak Ektar 100',
+    image: require('../../assets/films/ektar100/1.webp'),
+    specs: {
+      'film simulation': 'Classic Chrome',
+      'dynamic range': 'DR400',
+      'grain effect': 'Weak, Small',
+      'color chrome effect': 'Strong',
+      'color chrome fx blue': 'Strong',
+      'white balance': '4900K, +4 Red & -6 Blue',
+      highlight: '-1',
+      shadow: '-2',
+      color: '0',
+      sharpness: '-2',
+      nr: '-4',
+      clarity: '-3',
+      iso: 'Auto',
+      'exposure compensation': '+1'
+    },
+    images: [
+      require('../../assets/films/ektar100/1.webp'),
+      require('../../assets/films/ektar100/2.webp'),
+      require('../../assets/films/ektar100/3.webp'),
+      require('../../assets/films/ektar100/4.webp')
+    ]
+  },
 ]
