60b29c1aa0262a4c990a9e8b8f053facd46a8407
alvinle2901 <bach938@gmail.com>
2023-10-22 01:21:19 +0300

    done firebase config

---
+++ firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTA91ipP8kSUH3vnBaCBYBt7uujTQ5OBo",
+  authDomain: "fuji-recipe-react-native.firebaseapp.com",
+  projectId: "fuji-recipe-react-native",
+  storageBucket: "fuji-recipe-react-native.appspot.com",
+  messagingSenderId: "238693427724",
+  appId: "1:238693427724:web:161d21a63b3d6c92471c29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
