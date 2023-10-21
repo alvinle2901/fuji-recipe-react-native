91cf79f8c7957cdc19fadffa2e68d9a3244dfb74
alvinle2901 <bach938@gmail.com>
2023-10-21 09:50:28 +0300

    added component for itemInput

---
+++ src/components/InputItem.js
@@ -0,0 +1,32 @@
+import { Image, View, TextInput } from 'react-native'
+import React from 'react'
+
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from 'react-native-responsive-screen'
+
+const InputItem = ({ title, icon, handleChange, handleBlur, value }) => {
+  return (
+    <View
+      className="flex-row py-3 items-center"
+      style={{
+        borderRadius: 1,
+        borderBottomWidth: 1,
+        borderColor: '#f0eff2'
+      }}
+    >
+      <Image source={icon} style={{ height: wp(7), width: wp(7) }}></Image>
+      <TextInput
+        className="ml-3 w-full"
+        style={{ fontSize: wp(4) }}
+        onChangeText={handleChange}
+        onBlur={handleBlur}
+        value={value}
+        placeholder={title}
+      />
+    </View>
+  )
+}
+
+export default InputItem
