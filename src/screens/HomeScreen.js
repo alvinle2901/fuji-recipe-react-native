9cb01426c35d99c9b43150fd791be53004428760
alvinle2901 <bach938@gmail.com>
2023-10-24 01:00:36 +0300

    fetchData using firebase and handle search

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,10 +7,12 @@ import {
   TextInput
 } from 'react-native'
 import React, { useState } from 'react'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { MaterialIcons, FontAwesome } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { FunnelIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 import { StatusBar } from 'expo-status-bar'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../../firebase.config'
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
@@ -20,53 +22,102 @@ import Recipes from '../components/Recipes'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
-
   const [searchTerm, setSearchTerm] = useState('')
+  const [data, setData] = useState([])
+
+  // handle search function
   const handleSearchTerm = (text) => {
     setSearchTerm(text)
 
+    if (text != '') {
+      setData([
+        ...data.filter((item) =>
+          item.title.toLowerCase().includes(text.toLowerCase())
+        )
+      ])
+    } else {
+      setData(searchData)
+    }
+
     // setFiltered(feeds?.feeds.filter((item) => item.title.includes(text)));
   }
+  // fetch data from firebase
+  const fetchData = async () => {
+    const querySnapshot = await getDocs(collection(db, 'FujiRecipe'))
+    const recipes = []
+    querySnapshot.forEach((doc) => {
+      const recipe = doc.data()
+      recipes.push({
+        film_simulation: recipe.film_simulation,
+        dynamic_range: recipe.dynamic_range,
+        white_balance: recipe.white_balance,
+        color: recipe.color,
+        highlight: recipe.highlight,
+        shadow: recipe.shadow,
+        sharpness: recipe.sharpness,
+        noise_reduction: recipe.noise_reduction,
+        grain_effect: recipe.grain_effect,
+        color_chrome_fx: recipe.color_chrome_fx,
+        iso: recipe.iso,
+        exposure_compensation: recipe.exposure,
+        red: recipe.red,
+        blue: recipe.blue,
+        images: recipe.images,
+        title: recipe.title,
+        temp: recipe.temp,
+        favorite: recipe.favorite,
+        id: doc.id
+      })
+    })
+    setData(recipes)
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchData() // Fetch or update data every time the screen is focused
+    }, [])
+  )
 
   return (
     <SafeAreaView className="flex-1 bg-white">
       <StatusBar />
       <ScrollView showsVerticalScrollIndicator={false} className="space-y-6">
         {/* avatar */}
-        <View className="mx-5 flex-row justify-between items-center mb-3 mt-6">
+        <View className="mx-4 flex-row justify-between items-center mt-4 -mb-1">
           <Text
-            style={{ fontSize: wp(6) }}
-            className="font-bold text-neutral-700"
+            style={{ fontSize: wp(12.5), fontFamily: 'fin_thin', color: 'black' }}
           >
-            Fujifilm Recipes
+            fujifilm recipes
           </Text>
-          <TouchableOpacity onPress={() => navigation.navigate('Add')}>
+          <TouchableOpacity
+            className="p-3 rounded-full"
+            style={{ backgroundColor: '#f0eff2' }}
+            onPress={() => navigation.navigate('Add')}
+          >
             <Image
               source={require('../../assets/focus.png')}
-              style={{ height: wp(8), width: wp(8) }}
+              style={{ height: wp(5), width: wp(5)}}
             ></Image>
           </TouchableOpacity>
         </View>
         {/* Search Bar */}
-        <View className="flex-row items-center justify-between px-4 py-2 w-full space-x-6">
+        <View className="flex-row items-center justify-between px-4 pb-2 w-full space-x-6">
           <View className="px-4 py-2 bg-[#f0eff2] rounded-xl flex-1 flex-row items-center justify-center space-x-2">
-            <MaterialIcons name="search" size={24} color="#7f7f7f" />
+            <MagnifyingGlassIcon name="search" size={20} color="#7f7f7f" />
             <TextInput
               className="text-base text=[#555] flex-1"
-              placeholder="Search here..."
+              placeholder="Search..."
               value={searchTerm}
               onChangeText={handleSearchTerm}
             />
           </View>
-
           <TouchableOpacity className="w-10 h-10 rounded-xl flex items-center justify-center bg-[#f0eff2]">
-            <FontAwesome name="filter" size={24} color="#7f7f7f" />
+            <FunnelIcon name="filter" size={20} color="#7f7f7f" />
           </TouchableOpacity>
         </View>
-
         {/* Recipes */}
         <View>
-          <Recipes />
+          <Recipes data={data} />
         </View>
       </ScrollView>
     </SafeAreaView>
