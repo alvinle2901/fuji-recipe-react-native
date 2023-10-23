e568b4cf11eb9b316b0683820ec808699253ccd3
alvinle2901 <bach938@gmail.com>
2023-10-24 00:50:59 +0300

    added new font

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import AppNavigation from './src/navigation'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    fin_thin: require('./assets/finland_rounded_thin.ttf'),
+    'epic_fusion': require('./assets/EpicFusion.ttf')
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <AppNavigation />
     </GestureHandlerRootView>
   )
